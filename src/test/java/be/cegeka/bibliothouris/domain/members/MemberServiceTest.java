efbd0af99b6f986b202e2eb01ee26201491880b0
Paulien Lemay <paulien.lemay@cegeka.com>
2017-01-25 16:25:46 +0100

    aanzet test members

--- src/test/java/be/cegeka/bibliothouris/domain/members/MemberServiceTest.java
+++ src/test/java/be/cegeka/bibliothouris/domain/members/MemberServiceTest.java
@@ -1,54 +1,25 @@
 /*package be.cegeka.bibliothouris.domain.members;
 
-import org.junit.Before;
-import org.junit.Rule;
 import org.junit.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.MockitoJUnit;
-import org.mockito.junit.MockitoRule;
-import org.springframework.test.util.ReflectionTestUtils;
 
-import java.util.Arrays;
-import java.util.concurrent.atomic.AtomicLong;
+import static org.junit.Assert.*;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+/**
+ * Created by paulienl on 25/01/2017.
 
 public class MemberServiceTest {
+    MemberService memberService = new MemberService();
+    memberService.addMember(new Member("123", "Lemay", "paulien", "hendrik dewildestr", 24, 8501, "bissegem"));
+    memberService.addMember(new Member("123", "Devriend", "Jens", "eenStraat", 99, 9999, "Leuven"));
 
-    @Rule
-    public MockitoRule rule = MockitoJUnit.rule();
+    @Test
+    public void addMember() throws Exception {
 
-    @InjectMocks
-    private MemberService memberService;
-
-    @Mock
-    private MemberRepository memberRepository;
-
-    @Before
-    public void setUp() throws Exception {
-        ReflectionTestUtils.setField(memberService, "counter", new AtomicLong(4l));
     }
 
     @Test
-    public void addUser_ShouldCallUserRepository() throws Exception {
-        memberService.addMember("1", "lemay", "paulien", 24, 8501, "bissegem");
+    public void getAllMembers() throws Exception {
 
-        verify(memberRepository).addMember(new Member(5l, "Seppe"));
     }
 
-    @Test
-    public void getAllUsers() throws Exception {
-        Member member1 = new Member(1l, "Seppe");
-        Member member2 = new Member(2l, "Sanne");
-        Member member3 = new Member(3l, "Xan");
-
-        when(memberRepository.getAllMembers()).thenReturn(Arrays.asList(member1, member2));
-
-        assertThat(memberService.getAllMembers()).containsOnly(member1, member2);
-    }
-
-
-}*/
+} */
