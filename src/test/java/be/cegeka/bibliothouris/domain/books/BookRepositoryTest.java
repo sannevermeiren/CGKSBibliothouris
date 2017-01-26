d0663116d024482266612f2f0708e25354897a52
Paulien Lemay <paulien.lemay@cegeka.com>
2017-01-26 09:56:55 +0100

    testfix

--- src/test/java/be/cegeka/bibliothouris/domain/books/BookRepositoryTest.java
+++ src/test/java/be/cegeka/bibliothouris/domain/books/BookRepositoryTest.java
@@ -22,7 +22,7 @@ public class BookRepositoryTest {
         bookRepos.addBook(b1);
         bookRepos.addBook(b2);
         bookRepos.addBook(b3);
-        Assertions.assertThat(bookRepos.searchByISBN("886-53-798-6928-1")).isEqualTo("bookDetails\r\nisbn: 886-53-798-6928-1\r\ntitle: Een boek\r\nauthor first name: iemand\r\n");
+        Assertions.assertThat(bookRepos.searchByISBN("886-53-798-6928-1")).isEqualTo("bookDetails\r\nisbn: 886-53-798-6928-1\r\ntitle: Een boek\r\nauthor first name: voornaamiemand\r\nauthor last name: iemand\r\n");
     }
 
     @Test
@@ -31,7 +31,11 @@ public class BookRepositoryTest {
         bookRepos.addBook(b2);
         bookRepos.addBook(b3);
         bookRepos.addBook(b4);
-        Assertions.assertThat(bookRepos.searchByISBN("886-53-798")).isEqualTo("bookDetails\r\nisbn: 886-53-798-6928-1\r\ntitle: Een boek\r\nauthor: iemand\r\nbookDetails\r\nisbn: 886-53-798-7895-6\r\ntitle: een test boek\r\nauthor: van een auteur\r\n");
+        Assertions.assertThat(bookRepos.searchByISBN("886-53-798")).isEqualTo("bookDetails\r\n" +
+                "isbn: 886-53-798-6928-1\r\n" +
+                "title: Een boek\r\n" +
+                "author first name: voornaamiemand\r\n" +
+                "author last name: iemand\r\nbookDetails\r\nisbn: 886-53-798-7895-6\r\ntitle: een test boek\r\nauthor first name: voornaamEenAnder\r\nauthor last name: van een auteur\r\n");
     }
 
     @Test
